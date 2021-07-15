dbf7be320babca8e4ffe22a96c6897c90e88a0a0
Harsh <harsh.21483@gmail.com>
2021-07-15 04:22:13 -0700

    [Harsh]Add:-New class AddressBook is added

---
+++ AddressBookLinq/AddressBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookLinq
+{
+    class AddressBook
+    {
+        public void AddToDataTable()
+        {
+            // Create a new DataTable.
+            DataTable table = new DataTable();
+        }
+    }
+}
---
+++ AddressBookLinq/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBookLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address book Using linq ");
+            AddressBook addressBook = new AddressBook();
+        }
+    }
+}
